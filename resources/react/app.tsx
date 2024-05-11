d116f4013694f2391a55ba67947eb1b975fb24a6
mathieu <atsoumathieu228@gmail.com>
2024-05-11 16:00:49 +0100

    update

--- resources/react/app.tsx
+++ resources/react/app.tsx
@@ -1,8 +1,5 @@
-
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import Main from "./Main";
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('app')).render(
-    <Main />
-);
+ReactDOM.createRoot(document.getElementById("app")).render(<Main />);
